8ac58fd2b1a6a155d6c53938aa0180b492221b65
badogan <basri.dogan.71@gmail.com>
2020-01-08 16:47:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData(inputName,inputEmail) {
+
+let pElementInBody = document.getElementById("test-p")
+        
+
+let formData = {
+    name: inputName,
+    email: inputEmail
+  };
+   
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+   
+  fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+        pElementInBody.innerHTML = object.id
+    })
+    .catch(function(error){
+        let message = 'Unauthorized Access'
+        pElementInBody.innerHTML = error.message
+    });
+
+}
+
+submitData("Basri","b@email.com")
